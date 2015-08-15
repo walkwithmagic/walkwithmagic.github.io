bbea7901761c4b6ee6f6777a0ee626b9f48bc8fa
Boris Savic <boris.savic@xlab.si>
2015-08-15 12:15:57 +0200

    added files

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Place here your custom scripts
+ */
+
+
+$("#refferer").val(getUrlParameter('ref'))
+
+
+function success() {
+	$("#introText").hide();
+	$("#guestForm").hide();
+	$("#guestSuccess").fadeIn();
+
+
+}
+
+function submitData() {
+    $("#guestForm").submit();
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+
+    return "";
+}
